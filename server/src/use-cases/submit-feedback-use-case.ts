349b30a837ef4479d413a717f35a1bbd5fb928e7
William Martins <williammds00@gmail.com>
2022-05-05 13:38:18 -0300

    test: add jest unit testing

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -16,20 +16,36 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
 
+    if (!type) {
+      throw new Error('Type is required.')
+    }
+
+      if (!comment) {
+      throw new Error('Comment is required.')
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.')
+    }
+
     await this.feedbacksRepository.create({
       type,
       comment,
       screenshot,
     })
 
-    await this.mailAdapter.sendMail({
-      subject: 'Novo feedback',
-      body: [
-        `<div style="font-family: sans-serif; font-size: 16px; color: #111;`,
-        `<p>Tipo do feedback: ${type}</p>`,
-        `<p>Comentário: ${comment}</p>`,
-        `</div>`
-      ].join('\n')
-    })
+  await this.mailAdapter.sendMail({
+    subject: 'Novo feedback',
+    body: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+
   }
 }
+
+
+
