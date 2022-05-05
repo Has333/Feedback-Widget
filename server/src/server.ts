6dabafa33cec78038b60fc7de0ebc013e67850c6
William Martins <williammds00@gmail.com>
2022-05-05 11:14:52 -0300

    feat: add email feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
+
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d232549752d124",
+      pass: "5eb56d3ebcb8ce"
+    }
+  });
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ const feedback = await prisma.feedback.create({
         comment,
         screenshot,
     }
+})
+
+await transport.sendMail({
+    from: 'Equipe feedget <oi@feedget.com>',
+    to: 'William Martins <williammds00@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #222;">`,
+        `<p>Tipo do feedback: ${type}<p/>`,
+        `<p>Comentário: ${comment}<p/>`,
+        `</div>`
+    ].join('\n')
 })
 
 return res.status(201).json({data: feedback})
